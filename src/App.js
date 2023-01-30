7f07f538991bf27738864b11008b8364b866b676
athulpm <97athulpm@gmail.com>
2023-01-30 20:34:23 +0530

    Storing cart to DB and added UI notification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 
 function App() {
+  //store
+  const showCart = useSelector((state) => state.cart.showCart);
+  const items = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
 
-  const showCart = useSelector(state => state.cart.showCart);
-  const items = useSelector(state => state.cart.items);
+  //states
+  const [standBy, setStandBy] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failed, setFailed] = useState(false);
+
+  //side effects
+  useEffect(() => {
+    const putCartItems = async () => {
+      try {
+        setStandBy(true);
+        const response = await fetch(
+          "https://shopping-page-app-default-rtdb.firebaseio.com/cart.json",
+          {
+            method: "PUT",
+            body: JSON.stringify(items),
+            headers: {
+              "Content-Type": "application/JSON",
+            },
+          }
+        );
+        if(response.ok) {
+          setStandBy(false);
+          setSuccess(true);
+        }
+      } catch (err) {
+        setStandBy(false);
+        setFailed(true);
+        console.log(err);
+      }
+    };
+    putCartItems();
+  }, [items]);
+
   console.log(items);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <div>
+      {standBy && <Notification title="Sending..." message="Sending cart data!"/>}
+      {success && <Notification title="Success!" message="Sent cart data successfully!" status="success"/>}
+      {failed && <Notification title="Error!" message="Sending cart data failed!" status="error"/>}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </div>
   );
 }
 
