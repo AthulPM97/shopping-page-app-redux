7cf99691876660a2620006175366085f5e6cee55
athulpm <97athulpm@gmail.com>
2023-01-31 20:47:47 +0530

    implemented action creator thunk for get and put

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
+import { getData, sendCartData } from "./store/cart";
+
+let isInitial = true;
 
 function App() {
   //store
   const showCart = useSelector((state) => state.cart.showCart);
   const items = useSelector((state) => state.cart.items);
+  const notification = useSelector((state) => state.ui.notification);
   const dispatch = useDispatch();
 
-  //states
-  const [standBy, setStandBy] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [failed, setFailed] = useState(false);
-
-  //side effects
   useEffect(() => {
-    const putCartItems = async () => {
-      try {
-        setStandBy(true);
-        const response = await fetch(
-          "https://shopping-page-app-default-rtdb.firebaseio.com/cart.json",
-          {
-            method: "PUT",
-            body: JSON.stringify(items),
-            headers: {
-              "Content-Type": "application/JSON",
-            },
-          }
-        );
-        if(response.ok) {
-          setStandBy(false);
-          setSuccess(true);
-        }
-      } catch (err) {
-        setStandBy(false);
-        setFailed(true);
-        console.log(err);
-      }
-    };
-    putCartItems();
-  }, [items]);
+    if (isInitial) {
+      isInitial = false;
+      dispatch(getData());
+      return;
+    }
 
-  console.log(items);
+    dispatch(sendCartData(items));
+  }, [items, dispatch]);
 
   return (
     <div>
-      {standBy && <Notification title="Sending..." message="Sending cart data!"/>}
-      {success && <Notification title="Success!" message="Sent cart data successfully!" status="success"/>}
-      {failed && <Notification title="Error!" message="Sending cart data failed!" status="error"/>}
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Layout>
         {showCart && <Cart />}
         <Products />
